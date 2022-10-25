1102285cc278911530631cf516590dc09259f0aa
Hien <hien03kc@gmail.com>
2022-10-25 12:53:17 +0700

    update backend

---
+++ src/main/java/com/example/BachEnd_Ses4/BachEndSes4Application.java
@@ -0,0 +1,13 @@
+package com.example.BachEnd_Ses4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BachEndSes4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BachEndSes4Application.class, args);
+	}
+
+}
